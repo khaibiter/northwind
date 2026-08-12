bdc720e34879d382b7fda6a331d3173a32aebe3a
thanhkhai-car <thanhkhai.career@gmail.com>
2026-08-12 13:08:51 +0700

    Add .gitignore, example .env, and drizzle configuration; update package dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Mời bạn đến với lãnh địa của tôi nhé
+      </h1>
+    </main>
+  );
+};
+
+export default App;
